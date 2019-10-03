5c7b5a24d0f7ba495a07d471336ee87769ca842e
AndyMarg <margashov@gmail.com>
2019-10-03 21:31:30 +0300

    React functional added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import "@babel/polyfill";
+ import "@babel/polyfill";
 
-import './js/common';
-import './assets/css/main.css';
-import './assets/scss/main.scss';
+// import './assets/css/main.css';
+// import './assets/scss/main.scss';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/todo';
+
+ReactDOM.render(<App/>, document.getElementById("root"));
